b203857e79f060c558f9d636ac22a5c890c64d19
chaohai <2625218830@qq.com>
2023-09-07 21:36:07 +0800

    [fix] guessing_game-c in win

--- include/zetz/guessing_game.h
+++ include/zetz/guessing_game.h
@@ -1,7 +1,22 @@
 #ifndef ZETZ_GUESSING_GAME_H
 #define ZETZ_GUESSING_GAME_H
 
-void zetz_guessing_game_init();
-void zetz_guessing_game_run();
+#ifndef ZETZ_API_EXPORT
+#define ZETZ_API __declspec(dllexport)
+#else
+#define ZETZ_API __declspec(dllimport)
+#endif // ZETZ_API_EXPORT
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    ZETZ_API void zetz_guessing_game_init();
+    ZETZ_API void zetz_guessing_game_run();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // ZETZ_GUESSING_GAME_H
--- source-c/guessing_game/guessing_game.c
+++ source-c/guessing_game/guessing_game.c
@@ -2,29 +2,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 static int s_secret_number = 0;
 
-void zetz_guessing_game_init() {
-    s_secret_number = rand()*1.f / RAND_MAX * 100;
+void zetz_guessing_game_init()
+{
+    srand((unsigned)time(NULL));
+    s_secret_number = rand() % 100;
 }
 
-void zetz_guessing_game_run() {
+void zetz_guessing_game_run()
+{
     printf("Guess the number!\n");
 
     int guess = 0;
-    while (1) {
+    while (1)
+    {
         printf("Please input your guess.\n");
         scanf("%d", &guess);
 
         printf("You guessed: %d\n", guess);
 
-        if (guess == s_secret_number) {
+        if (guess == s_secret_number)
+        {
             printf("You win!\n");
             break;
-        } else if (guess < s_secret_number) {
+        }
+        else if (guess < s_secret_number)
+        {
             printf("Too small!\n");
-        } else {
+        }
+        else
+        {
             printf("Too big!\n");
         }
     }
