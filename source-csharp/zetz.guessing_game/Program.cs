0d7788c4c68354287af00777d2ecaba1f1c518ad
chaohai <2625218830@qq.com>
2023-09-07 20:12:14 +0800

    [feat] add csharp guessing_game

---
+++ source-csharp/zetz.guessing_game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace zetz
+{
+    class GuessingGame
+    {
+        private int secretNumber;
+
+        public void InitGame()
+        {
+            secretNumber = new Random().Next(1, 101);
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Guess the number!");
+
+            int guess;
+            while (true)
+            {
+                Console.WriteLine("Please input your guess.");
+
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Too small!");
+                }
+                else
+                {
+                    Console.WriteLine("Too big!");
+                }
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, world!");
+
+            var game = new GuessingGame();
+            game.InitGame();
+            game.Run();
+        }
+    };
+}
